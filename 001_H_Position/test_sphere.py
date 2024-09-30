8851e07fa82244b916d2cdf5d5705791e006a448
Timm Lankau <twalankau@mx.nthu.edu.tw>
2024-09-30 07:01:36 +0000

    240930 - 15:00

--- 001_H_Position/test_sphere.py
+++ 001_H_Position/test_sphere.py
@@ -16,5 +16,37 @@ sys.path.append('/dicos_ui_home/tlankau/QMMM/Q15_CCDC')
 from qmmm import printf
 from qmmm import fprintf
 
-printf("Experiment to scan a shere for the position\n")
+inpf="/dicos_ui_home/tlankau/Structures/TingYi_Data/ABASEE_1/ABASEE-oxo_1_1_tpss_d4_lanl2dz_631gpol_Co3.opt.xyz"
+
+def scan_sphere(at, rad):
+  return()
+
+printf("Read input file\n")
+obConversion = ob.OBConversion()
+obConversion.SetInAndOutFormats("xyz", "xyz")
+obmol = ob.OBMol()
+obConversion.ReadFile(obmol, inpf)
+atom_number = obmol.NumAtoms()
+printf("%i atoms read\n", atom_number)
+printf("Search for MeO pairs\n")
+
+for at in ob.OBMolAtomIter(obmol):
+  if at.GetAtomicNum() >= 19:
+    metal_idx = at.GetIdx()
+    metal_symbol = ob.GetSymbol(at.GetAtomicNum())
+    printf("%s found at index: %i\n", metal_symbol, metal_idx)
+    break
+for nat in ob.OBAtomAtomIter(at):
+  ox_idx = nat.GetIdx()
+  if nat.GetAtomicNum() == 8:
+    printf("O atom at index %i\n", ox_idx)
+    bond = at.GetBond(nat)
+    bond_order = bond.GetBondOrder()
+    bond_length = bond.GetLength()
+    printf("The bond order between both is: %i\n", bond_order)
+    printf("%sO bond length: %5.3f A\n", metal_symbol, bond_length)
+    break
+
+rad = 1.5
+printf("Scan O-%i with a radius of %f A\n", ox_idx, rad)
 exit()
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Volcano_TS
+- The original volcano data are not part of the repository to make tracking easier.
+  /dicos_ui_home/tlankau/Structures/TingYi_Data
+- 001_H_Position
+  A collection of scripts to test methods to find the best position for the H atom of the CH bond.
+  test_sphere.py    Find the point with the least amount of interactions with cluster on a sphere
+                    around the oxygen atom.
