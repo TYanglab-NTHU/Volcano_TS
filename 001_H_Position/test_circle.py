f1e9ac6cb30044fd6fd1c04d8d32be092bc67622
Timm Lankau <twalankau@mx.nthu.edu.tw>
2024-10-01 09:18:01 +0000

    241001 - 17.15

--- 001_H_Position/test_circle.py
+++ 001_H_Position/test_circle.py
@@ -28,6 +28,15 @@ def get_bond_vec(mol, at1, at2):
                   at2.GetZ() - at1.GetZ()])
   return(vec)
 
+def contribution(p1, p2):
+  contrib = 0.0
+  d  = (p1[0]-p2[0])*(p1[0]-p2[0])
+  d += (p1[1]-p2[1])*(p1[1]-p2[1])
+  d += (p1[2]-p2[2])*(p1[2]-p2[2])
+  d  = ma.sqrt(d)
+  contrib = 1.0 / d
+  return(contrib)
+
 ################################################################################
 
 def vec_angle(v1, v2):
@@ -42,6 +51,7 @@ def vec_axis(v1, v2):
   return(rv)
 
 # a: normalized rotational axis, t: rotational angle
+# https://en.wikipedia.org/wiki/Rotation_matrix
 def build_rot_mat(a, t):
   st = ma.sin(t)
   ct = ma.cos(t)
@@ -56,6 +66,18 @@ def build_rot_mat(a, t):
                    a[1]*a[2] * mt + a[0]*st,
                    a[2]*a[2] * mt +      ct]])
   return(mat)
+
+################################################################################
+
+# https://en.wikipedia.org/wiki/Spherical_coordinate_system
+# r: radius, t: theta angle in xy plane, p: phi angle to the zaxis
+# r: 0 to inf, t: 0 to 360, p: 0 to 180
+# r in Angs, t and p in radian
+def sphere_to_xyz(r, t, p):
+  tx = r * ma.sin(p) * ma.cos(t)
+  ty = r * ma.sin(p) * ma.sin(t)
+  tz = r * ma.cos(p)
+  return([tx, ty, tz])
 
 ################################################################################
 
@@ -156,8 +178,74 @@ print()
 
 # write xyz file with the result from the rotation
 print("Write rotated molecule to disk")
+obmol.SetTitle("molecule after translation and rotation")
 obConversion.SetOutFormat("xyz")
 xyz_file = "./test_circle_01.xyz"
+obConversion.WriteFile(obmol, xyz_file)
+print()
+
+# draw a circle of dummy atoms
+print("Add a circle of dummy atoms to the molecule")
+for theta in range(0, 360, 10):
+  printf("theta: %5.1f\n", theta)
+  coor = sphere_to_xyz(1.5, ma.radians(theta), ma.radians(180.0-109.47))
+  # add a dummy atom to the molecule
+  new_atom = obmol.NewAtom()
+  new_atom.SetAtomicNum(0)
+  new_atom.SetVector(coor[0], coor[1], coor[2])
+print()
+
+# write xyz file with the dummy atom
+print("Write augmented molecule to disk")
+obmol.SetTitle("molecule with a ring of dummy atoms")
+obConversion.SetOutFormat("xyz")
+xyz_file = "./test_circle_02.xyz"
+obConversion.WriteFile(obmol, xyz_file)
+print()
+
+# delete the dummy atoms for a fresh start
+del_at=bool(True)
+while del_at == bool(True):
+  del_at=bool(False)
+  for at in ob.OBMolAtomIter(obmol):
+    if at.GetAtomicNum() == 0:
+      obmol.DeleteAtom(at)
+      del_at=bool(True)
+      break
+
+# do a circle scan
+print("Search for the best point on the circle")
+rs = 10000.0 # final rsults sum
+rl = []      # final results vector
+for theta in range(0, 360, 2):
+  # pick a point on the circle
+  pt = sphere_to_xyz(1.5, ma.radians(theta), ma.radians(180.0-109.47))
+  # calculate sum
+  sum = 0.0
+  for at in ob.OBMolAtomIter(obmol):
+    if at == oxy: continue
+    sum += contribution([at.GetX(), at.GetY(), at.GetZ()], pt)
+  if sum < rs:
+    print(sum, rs)
+    rs = sum
+    rl = pt
+if rl == []:
+  print("Locating the H position failed")
+print("Best point found")
+printf("Sum     : %6.4f\n", rs)
+print ("Position:", rl)
+print()
+
+# add the optimized point as dummy to the molecule
+new_atom = obmol.NewAtom()
+new_atom.SetAtomicNum(0)
+new_atom.SetVector(rl[0], rl[1], rl[2])
+
+# write xyz file with the optimized dummy atom
+print("Write optimization test to disk")
+obmol.SetTitle("molecule with best H atom position")
+obConversion.SetOutFormat("xyz")
+xyz_file = "./test_circle_03.xyz"
 obConversion.WriteFile(obmol, xyz_file)
 
 exit()
